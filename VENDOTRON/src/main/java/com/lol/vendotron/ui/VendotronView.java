e435f2535b4ecbade1516d76d0f1888a013a27d2
magpantaynic <20709370+magpantaynic@users.noreply.github.com>
2022-04-12 13:37:12 -0400

    view class

--- VENDOTRON/src/main/java/com/lol/vendotron/ui/VendotronView.java
+++ VENDOTRON/src/main/java/com/lol/vendotron/ui/VendotronView.java
@@ -4,10 +4,62 @@
  */
 package com.lol.vendotron.ui;
 
+import java.util.List;
+import com.lol.vendotron.dao.VendotronDaoFileException;
+import com.lol.vendotron.dto.Egg;
+
 /**
  *
- * @author Henry
+ * @author Nicole
  */
-public class VendotronView {
+
+public class VendotronView 
+{
+    private UserIO io = new UserIOConsoleImpl();
+}
+
+public int getMenuSelection()
+{
+    io.print("Main Menu");
+    io.print("1. Put Money To Buy");
+    io.print("2. Select Item");
+    io.print("3.");
+    io.print("4. Cancel");
+    io.print("5.");
     
-    }
+    return io.readInt("Please choose one of the options above:")    
+}
+
+// Display 
+public void displayselectItem()
+{
+    io.print();
+}
+
+
+// Display Banners
+
+public void display()
+{
+    io.print("---Buy an Item---");
+}
+
+public void display()
+{
+    io.print("---Insufficient Funds---");
+}
+
+public void displayCancel()
+{
+    io.print("---Dispensing Egg---");
+}
+
+public void displayunknownCommand()
+{
+    io.print("---Unknown Command---");
+}
+
+public void displayexitMessage()
+{
+    io.print("---Bye!---");
+}
